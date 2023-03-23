a8b60f599886ad16f9a891eb3cdeb37f219c58eb
Mariana Lobo <m.lobocasas@gmail.com>
2023-03-23 12:07:36 +0100

    add responsive styles and status icons

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -3,6 +3,8 @@ import grif from "../images/grif.png";
 import sli from "../images/sli.png";
 import huf from "../images/huf.png";
 import rav from "../images/rav.png";
+import dead from "../images/dead.svg";
+import alive from "../images/alive.svg"
 import "../styles/CharacterDetail.scss";
 import { Link } from 'react-router-dom';
 
@@ -23,22 +25,34 @@ function CharacterDetail({ characterFind }) {
 
   
   return (
+    <section class='container-details'>
     <div className="character-detail">
       <img
-        className="character-img"
+        className="img-detail"
         src={characterFind.image ? characterFind.image : imgDefault}
         alt="imagen"
       ></img>
       <div className="info-detail">
         <p className="text-detail">Nombre: {characterFind.name}</p>
         <p className="text-detail"> Especie: {characterFind.species} </p>
-        <p className="text-detail">casa: {characterFind.house}</p>{characterFind.alive === true ? <p className="text-detail">Estado: Alive</p> : <p className="text-detail">Estado: Dead</p>}
+        <p className="text-detail">casa: {characterFind.house}</p>
+        {characterFind.alive === true ?
+          <div className='status'>
+            <p className="text-detail">Estado: Alive</p>
+            <img className='icon-alive' src={alive} alt='corazón con latido'></img>
+            </div>
+          :
+          <div className='status'>
+            <p className="text-detail">Estado: Dead</p>
+            <img className='icon-alive' src={dead} alt='calavera'></img>
+          </div>}
         <p className="text-detail">Género: {characterFind.gender}</p>
         <p className="text-detail">Nombre alternativo: {characterFind.altName}</p>
       </div>
           {houseImg()};
           <Link className='return' to="/">Volver</Link>
-    </div>
+      </div>
+      </section>
   );
 }
 
